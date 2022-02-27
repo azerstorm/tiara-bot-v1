e2b47c75cb585dd00559fe6125a424782d64704c
unknown <rezathegamerz@gmail.com>
2022-02-27 20:12:58 +0700

    better life

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 "# tiara-bot-v1" 
 "# tiara-bot-v1" 
+"# tiara-bot-v1" 
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import CallbackContext
 from config import TOKEN
+from config import PORT
 
 def start(update:Update, context:CallbackContext):
-    update.message.reply_text("Hi ayang {}".format(update.message.from_user.username))
+    update.message.reply_text("Hi ayang {} kamu sehat kan?".format(update.message.from_user.username))
 
 if __name__ == '__main__':
     updater = Updater(TOKEN)
@@ -13,5 +14,5 @@ if __name__ == '__main__':
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    updater.start_polling()
+    updater.start_webhook("0.0.0.0", PORT, TOKEN, webhook_url='https://tiarabot.herokuapp.com/'+TOKEN)
     updater.idle()
