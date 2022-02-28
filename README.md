0154c8ee7a4e2a9b93ce2d1c72d1665ec9621c00
unknown <rezathegamerz@gmail.com>
2022-02-28 13:13:23 +0700

    better life

--- README.md
+++ README.md
@@ -37,3 +37,4 @@
 "# tiara-bot-v1" 
 "# tiara-bot-v1" 
 "# tiara-bot-v1" 
+"# tiara-bot-v1" 
--- calendarmessages.py
+++ calendarmessages.py
@@ -2,4 +2,4 @@ CALENDAR_CALLBACK = "CALENDAR"
 
 calendar_message = "Kapan kamu mau dating sama aku? 😊"
 
-calendar_response_message = "You selected %s"
+calendar_response_message = "Kamu pilih tanggal %s"
