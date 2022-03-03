f559a68599413804d3bde4b7b64d8a83cf94d308
unknown <rezathegamerz@gmail.com>
2022-03-03 16:25:34 +0700

    better life

--- converstion.py
+++ converstion.py
@@ -1,10 +1,17 @@
 from datetime import datetime
+import re
 
 
 def sample_response(input_text):
     user_message = str(input_text).lower()
 
-    if user_message.contains() in ("hello", "hi", "hai", "uy", "uyy", "uyyy", "woy", "zey", "sayang", "sayangku", "sayang sayang", "darling", "my darling", "beb", "bebeb", "honey"):
+    badWordList = ["anj", "tai", "anjing", "pantek", "bangsat"]
+
+    for word in badWordList:
+        if re.findall(word, user_message):
+            return "Sayang, jangan dibiasain toxic 😠"
+
+    if user_message in ("hello", "hi", "hai", "uy", "uyy", "uyyy", "woy", "zey", "sayang", "sayangku", "sayang sayang", "darling", "my darling", "beb", "bebeb", "honey"):
         return "Iya sayang, ada apa? 😊"
 
     if user_message in ("sehat", "sehat sayang", "sehat dong", "sehat dunds", "alhamdulillah sehat", "aman"):
@@ -13,7 +20,7 @@ def sample_response(input_text):
     if user_message in ("kamu udah makan?", "kamu udah makan say?", "kamu udah makan sayang?", "udah makan belum?"):
         return "Udah dong sayang 😊"
     
-    if user_message in ("iya maaf", "iya maaf sayang"):
+    if user_message in ("iya maaf", "iya maaf sayang", "eh iya maaf sayang"):
         return "Chill sayang chill 🤗"
 
     if user_message in ("kamu dimana?", "kamu dimana sayang?"):
