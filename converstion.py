093757227f2ac8c841bbc6aa31833b088524e511
unknown <rezathegamerz@gmail.com>
2022-03-03 13:50:50 +0700

    better life

--- converstion.py
+++ converstion.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def sample_response(input_text):
     user_message = str(input_text).lower()
 
-    if user_message.contains("hello", "hi", "hai", "uy", "uyy", "uyyy", "woy", "zey", "sayang", "sayangku", "sayang sayang", "darling", "my darling", "beb", "bebeb", "honey"):
+    if user_message.contains() in ("hello", "hi", "hai", "uy", "uyy", "uyyy", "woy", "zey", "sayang", "sayangku", "sayang sayang", "darling", "my darling", "beb", "bebeb", "honey"):
         return "Iya sayang, ada apa? 😊"
 
     if user_message in ("sehat", "sehat sayang", "sehat dong", "sehat dunds", "alhamdulillah sehat", "aman"):
